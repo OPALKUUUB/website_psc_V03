f2213956aff2c3d36783594b5988b83c21c2f0c3
OPALKUUUB <63914959+OPALKUUUB@users.noreply.github.com>
2021-10-01 09:31:15 +0700

    style header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -5,40 +5,49 @@ class Header extends Component {
   render() {
     return (
       <div className="Header">
-        <div className="Header-content">
-          <div className="Header-contact">
-            <div className="contact-left">address content</div>
-            <div className="contact-right">phone content</div>
-          </div>
-          <div className="Header-logo">
-            <h1>psc</h1>
-          </div>
-          <div className="Header-navbar">
-            <ul>
-              <li>
-                <a href="/#">home</a>
-              </li>
-              <li>
-                <a href="/#">about us</a>
-              </li>
-              <li>
-                <a href="/#">contacts</a>
-              </li>
-            </ul>
-          </div>
-          <div className="Header-body">
-            <div className="body-head">
-              <h1>
-                we make shipping
-                <br />
-                big stuff easy
-              </h1>
+        <div className="Header-frame">
+          <div className="Header-content">
+            <div className="Header-contact">
+              <div className="contact-left">
+                <p>address content</p>
+              </div>
+              <div className="contact-right">
+                <p>phone content</p>
+              </div>
             </div>
-            <div className="body-tail">
-              <h3>Join with us getting bargain deals on shipping container</h3>
+            <div className="Header-logo">
+              <h1>psc</h1>
             </div>
-            <div className="content-button">
-              <a href="/#">contact us</a>
+            <hr className="Header-bar" />
+            <div className="Header-navbar">
+              <ul className="navbar-nav">
+                <li>
+                  <a href="/#">home</a>
+                </li>
+                <li>
+                  <a href="/#">about us</a>
+                </li>
+                <li>
+                  <a href="/#">contacts</a>
+                </li>
+              </ul>
+            </div>
+            <div className="Header-body">
+              <div className="body-head">
+                <h1>
+                  we make shipping
+                  <br />
+                  big stuff easy
+                </h1>
+              </div>
+              <div className="body-tail">
+                <h3>
+                  Join with us getting bargain deals on shipping container
+                </h3>
+              </div>
+              <div className="content-button">
+                <a href="/#">contact us</a>
+              </div>
             </div>
           </div>
         </div>
